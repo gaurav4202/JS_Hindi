e9362ba537411855e6c7970448b9f68f72ce4abe
Gaurav Vishwakarma <136454331+gaurav4202@users.noreply.github.com>
2024-10-06 10:37:01 +0000

    Conversion of operation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,36 +1,35 @@
-let score = "Gaurav"
+let score = "Gaurav";
 
 //console.log(typeof score);
 //console.log(typeof(score));
 
-let valueInNumber = Number(score)
+let valueInNumber = Number(score);
 //console.log(typeof valueInNumber);
 //console.log(valueInNumber);
-
 
 // "33" => 33
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "Gaurav"
+let isLoggedIn = "Gaurav";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn);
 //  console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "Gaurav" => true
 
-let someNumber = 33
+let someNumber = 33;
 
-let stringNumber = String(someNumber)
+let stringNumber = String(someNumber);
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 
 // *********************** Operations ***********************
 
-let value = 3
-let negValue = -value
+let value = 3;
+let negValue = -value;
 // console.log(negValue);
 
 // console.log(2+2);
@@ -40,16 +39,17 @@ let negValue = -value
 // console.log(2/3);
 // console.log(2%3);
 
-let str1 = "Hello"
-let str2 = " Gaurav"
+// let str1 = "Hello!";
+// let str2 = " Gaurav";
+// let str3 = " Vishwakarma";
 
-let str3 = str1 + str2
-console.log(str3);
+// let str4 = str1 + str2 + str3;
+// console.log(str4);
 
-console.log("1" + 2);
-console.log(1 + "2");
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2");
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
 
 // console.log( (3 + 4) * 5 % 3);
 
